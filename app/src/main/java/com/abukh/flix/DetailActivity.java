100da12bc4c7990e20d98dce59202c37cffcd51b
Mohammed Abu Bakar Khan <abukhan363@gmail.com>
2021-02-22 11:49:09 -0600

    Update DetailActivity.java

--- app/src/main/java/com/abukh/flix/DetailActivity.java
+++ app/src/main/java/com/abukh/flix/DetailActivity.java
@@ -24,7 +24,7 @@ import org.parceler.Parcels;
 
 public class DetailActivity extends YouTubeBaseActivity {
 
-    private static final String YOUTUBE_API_KEY = "AIzaSyAjLqr22HpvF56-Zab_0NachOUbY_-7WrU";
+    private static final String YOUTUBE_API_KEY = // YOUR API KEY HERE;
     private static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
     private int isPopularMovie = 0;
     TextView tvTitle;
